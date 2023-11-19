033146bfb3c87938f400528b3acf4bf1de4c2c63
NatchaphonFont <natchaphon4474@gmail.com>
2023-11-19 14:08:14 +0700

    sdd
    ddd

---
+++ MainH/JSPAY.js
@@ -0,0 +1,5 @@
+<script>
+  document.getElementById('checkAvailabilityBtn').addEventListener('click', function() {
+    window.location.href = './pay.html'; // เปิดหน้า pay.html เมื่อคลิกที่ปุ่ม
+  });
+</script>
